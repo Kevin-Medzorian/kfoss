31f3c995ede32c051afa571233216311a222169f
Kevin Medzorian <kevin.medzor@gmail.com>
2023-04-21 14:17:56 -0700

    optimized images, converted logos to vectors

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,16 +3,11 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgFor, NgIf } from '@angular/common';
 import { MainComponent } from 'src/app/main/main.component';
 import { FadeIn, FadeOut } from 'src/app/shared/animations';
+import { tools } from 'src/app/shared/models/tools.model';
 import texture_index from 'src/assets/images/textures/index.json';
 
 const DELTA=200;
 
-interface Tool {
-  title: string;
-  text: string;
-  hov?: boolean | number;
-  url: string;
-}
 
 interface Texture {
   url?: string;
@@ -30,19 +25,13 @@ interface Texture {
 })
 export class HomeComponent {
 
-  tools: Tool[] = [
-    {title: 'Spanish Accents Tool', text: 'Simple, always-visible accents utility for typing spanish accent characters.', url: this.app.baseHref + 'spanish-accents'},
-    {title: 'Lightweight Screenshot Utility', text: "Minimal screenshot tool called 'Screencapper'.", url: this.app.baseHref + 'screencapper'},
-    {title: 'Automatic Definition Finder', text: 'Given a list of words, this tool returns a list of definitions using the internet.', url: this.app.baseHref + 'definition-finder'},
-    {title: 'Secure Text/Password Encrypter', text: 'Simple, lightweight C+Qt program that will encrypt/decrypt text using a provided hash.', url: this.app.baseHref + 'encrypter'},
-    {title: 'Ad-Free Android Metronome', text: 'Simple, ad-free Android Metronome app.', url: this.app.baseHref + 'metronome'},
-  ];
+  tools = tools;
   window = window;
 
   textures: Texture[] = [];
   imagePath = this.app.baseHref + 'assets/images/';
   rawTexturePath = this.imagePath + 'textures/raw/';
-	images = ['sample-code.png', 'sample-unity.png', 'sample-smss-inverted.png'].map((n) => this.imagePath + n);
+	images = ['sample-code.jpg', 'sample-unity.jpg', 'sample-smss-inverted.jpg'].map((n) => this.imagePath + n);
 
   constructor(public app: MainComponent){}
 
@@ -51,10 +40,10 @@ export class HomeComponent {
   }
 
   preloadTextures() {
-    const texture_urls = texture_index.images.map(x => this.imagePath + 'textures/' + x);
-    const promises: Promise<Texture>[] = texture_urls.map(x => { return this.getBase64ImageFromUrl(x) });
+    const texture_urls: string[]           = texture_index.images.map(x => this.imagePath + 'textures/' + x);
+    const promises:     Promise<Texture>[] = texture_urls.map(x => this.getBase64ImageFromUrl(x));
 
-    Promise.all(promises).then((textures) => {
+    Promise.all(promises).then(textures => {
       this.textures = textures;
     });
   }
@@ -65,9 +54,9 @@ export class HomeComponent {
   
     return new Promise((resolve, reject) => {
       const reader  = new FileReader();
-      reader.addEventListener("load", function () {
+      reader.addEventListener("load", () => {
           const result: Texture = {url: reader.result?.toString(),
-                                   fileName: imageUrl.split("/").pop()?.split(".")[0] + '.png'};
+                                   fileName: imageUrl.split("/").pop()?.replace('.jpg', '.png')};
           resolve(result);
       }, false);
   
--- src/app/shared/models/tools.model.ts
+++ src/app/shared/models/tools.model.ts
@@ -1,18 +1,52 @@
-interface Tool {
+export interface Tool {
     name: string;
     shortName: string;
+    desc: string;
     route: string;
-    imageUrls: string[];
-    text: string[];
+    images: string[];
+    text: string[]; // Array of text items
+    hov?: boolean | number; // Hovered or Not
 }
 
 export const tools: Tool[] = [
     {
         name: 'Spanish Accents Tool',
         shortName: 'Spanish Accents',
+        desc: 'Simple, always-visible accents utility for typing spanish accent characters.',
         route: 'accents',
-        imageUrls: [],
+        images: [],
+        text: []
+    },
+    {
+        name: 'Lightweight Screenshot Utility',
+        shortName: 'Screencapper',
+        desc: "Minimal screenshot tool called 'Screencapper'.",
+        route: 'screencapper',
+        images: [],
+        text: []
+    },
+    {
+        name: 'Automatic Definition Finder',
+        shortName: 'Definition Finder',
+        desc: "Given a list of words, this tool returns a list of definitions using the internet.",
+        route: 'definition-finder',
+        images: [],
+        text: []
+    },
+    {
+        name: 'Secure Text/Password Encrypter',
+        shortName: 'Text Encrypter',
+        desc: "Simple, lightweight C+Qt program that will encrypt/decrypt text using a provided hash.",
+        route: 'encrypter',
+        images: [],
+        text: []
+    },
+    {
+        name: 'Ad-Free Android Metronome',
+        shortName: 'Metronome App',
+        desc: "Simple, ad-free Android Metronome app.",
+        route: 'metronome',
+        images: [],
         text: []
     }
-    //const tools = ['accents', 'metronome', 'definition-finder', 'encrypter', 'screencapper'];
 ];
