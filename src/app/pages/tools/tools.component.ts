c28f4941cf42051e5c7ae43ea1f7822495a34bc0
Kevin Medzorian <kevin.medzor@gmail.com>
2023-06-06 13:51:53 -0700

    Added GNU GPLv3, updated footer

--- src/app/pages/tools/tools.component.ts
+++ src/app/pages/tools/tools.component.ts
@@ -1,8 +1,13 @@
+/**
+ * Copyright 2023, Kevin Medzorian 
+ * This file is part of KFOSS.
+ * KFOSS is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+ * KFOSS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with KFOSS. If not, see <https://www.gnu.org/licenses/>.
+ **/
 import { Component } from '@angular/core';
-import { NavigationEnd, Router, RouterEvent, Event } from '@angular/router';
+import { Router} from '@angular/router';
 import { tools, Tool } from 'src/app/shared/models/tools.model';
-import { menu } from 'src/app/shared/models/dropdown.model';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-tools',
