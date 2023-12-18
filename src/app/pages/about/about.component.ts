ab11a582635c689a139e21de88e6439b3f3516be
Kevin Medzorian <kevin.medzor@gmail.com>
2023-12-18 01:47:38 -0800

    Fixed randomization icons

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -5,7 +5,7 @@
  * KFOSS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with KFOSS. If not, see <https://www.gnu.org/licenses/>.
  **/
-import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import logo_index from 'src/assets/images/tech/index.json';
 
 @Component({
@@ -17,40 +17,54 @@ export class AboutComponent {
   @ViewChildren("images") images: QueryList<ElementRef> | undefined;
   @ViewChild("page") page: ElementRef | undefined;
   
+  public resizeTimeout: any;
+  public touched = false;
   public logos = logo_index.icons.sort(() => Math.random() - 0.5).sort(() => Math.random() - 0.5);;  // Randomized
   document = document;
 
   ngAfterViewInit(): void {
-      let pageHeight = this.page!.nativeElement.clientHeight;
-      let x = 50, y = 50;
-      const x_increment = 350;
-      const y_increment = 250;
-      const marginSize = (1/3)*this.document.body.clientWidth;
-      const middleSize = this.document.body.clientWidth - (2*marginSize);
-      this.images!.forEach(img => {
-        console.log(img)
-        if (x > this.document.body.clientWidth - 75) {
-          x = 50;
-          y += y_increment;
-        }
+    this.randomlyLayoutIcons();
+  }
 
-        if (y > pageHeight) {
-          img.nativeElement.style.display = "none";
-        }
+  @HostListener('window:resize', ['$event'])
+  resized() {
+    clearTimeout(this.resizeTimeout);
+    this.resizeTimeout = setTimeout(() => this.randomlyLayoutIcons(), 100);
+  }
+  
+  randomlyLayoutIcons(): void {
+    let pageHeight = this.page!.nativeElement.clientHeight;
+    let x = 50, y = 50;
+    const x_increment = 350;
+    const y_increment = 250;
+    const marginSize = (1/3)*this.document.body.clientWidth;
+    const middleSize = this.document.body.clientWidth - (2*marginSize);
+    this.images!.forEach(img => {
+      console.log(img)
+      if (x > this.document.body.clientWidth - 75) {
+        x = 50;
+        y += y_increment;
+      }
 
-        img.nativeElement.style.top = y + (Math.random() * 100) - 50 +"px";
-        img.nativeElement.style.left = x + (Math.random() * 100) - 50 +"px";
-        let random = (Math.random() * 80) - 40;
-        if (Math.abs(random) < 5) {
-          random = (Math.random() * 80) - 40;
-        }
+      if (y > pageHeight) {
+        img.nativeElement.style.display = "none";
+      } else {
+        img.nativeElement.style.display = "unset";
+      }
 
-        (img.nativeElement as HTMLImageElement).style.transform = `rotate(${random}deg)`;
-        x += x_increment;
+      img.nativeElement.style.top = y + (Math.random() * 100) - 50 +"px";
+      img.nativeElement.style.left = x + (Math.random() * 100) - 50 +"px";
+      let random = (Math.random() * 80) - 40;
+      if (Math.abs(random) < 5) {
+        random = (Math.random() * 80) - 40;
+      }
 
-        if (x > marginSize + 50 && x < marginSize + middleSize - 150) {
-          x = marginSize + middleSize;
-        }
-      });
-    }
+      (img.nativeElement as HTMLImageElement).style.transform = `rotate(${random}deg)`;
+      x += x_increment;
+
+      if (x > marginSize + 50 && x < marginSize + middleSize - 150) {
+        x = marginSize + middleSize;
+      }
+    });
+  }
 }
